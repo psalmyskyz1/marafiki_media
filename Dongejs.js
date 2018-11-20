1b6a901067878345bc6499f77d4fca3145e71256
sammy <sklagat46@gmail.com>
2018-11-20 16:08:09 +0300

    added some files

---
+++ Dongejs.js
@@ -0,0 +1,9 @@
+
+  function validateForm() {
+  var x = document.form.firstname.value;
+  if (x == "") {
+      alert("Name must be filled out");
+      return false;
+  }
+}
+document.write(i am sammy);
